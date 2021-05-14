585b872d2b60bfe3dbc58d52ac8e13eca02c7011
KHJ <rjarorgudwns@naver.com>
2021-05-14 19:17:58 +0900

    hi

---
+++ ingil/src/menu/InsertPage.java
@@ -0,0 +1,71 @@
+package menu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import main.MenuMain;
+
+public class InsertPage extends JFrame implements ActionListener{
+	
+	 JTextField txt1;
+	 JTextField txt2;
+	 JTextField txt3;
+	 JButton insertBtn;
+	
+	 MenuDAO dao = new MenuDAO();
+	
+	 
+	
+	 public void InsertPage() {
+		JPanel jpaenl = new JPanel();
+		
+		jpaenl.add(new JLabel("상품 코드"));
+		jpaenl.add(txt1 = new JTextField(10));
+		jpaenl.add(new JLabel("상 품 명"));
+		jpaenl.add(txt2 = new JTextField(10));
+		jpaenl.add(new JLabel("가 격"));
+		jpaenl.add(txt3 = new JTextField(10));
+		
+		JPanel JPaenlBtn = new JPanel();
+		JPaenlBtn.add(insertBtn = new JButton("추가"));
+		add(JPaenlBtn,"South");
+		add(jpaenl);
+		
+		insertBtn.addActionListener(this);
+		
+		
+		addWindowListener(new WindowAdapter() {
+			public void windowClosed(WindowEvent e) {
+				new MenuMain();
+			}
+		});
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(350,250,530,100);
+		setVisible(true);
+		
+	}
+	 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == insertBtn) {
+			 String sur1 = txt1.getText();
+			String sur2 = txt2.getText();
+			Integer sur3 = Integer.parseInt(txt3.getText());
+			dao.InsertCname(sur1, sur2, sur3);
+			
+			JOptionPane.showMessageDialog(null, "추가 성공");
+			txt1.setText("");
+			txt2.setText("");
+			txt3.setText("");
+		}
+	}
+}
---
+++ ingil/src/menu/MenuDAO.java
@@ -0,0 +1,113 @@
+package menu;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.table.DefaultTableModel;
+
+public class MenuDAO {
+	Connection con;
+	 PreparedStatement pstmt;  
+	 ResultSet rs;
+	 Statement stmt;
+	 String sql; 
+	
+	
+	 
+	 public	MenuDAO() {
+		 try {
+			connectDB();
+			System.out.println("111");
+		}catch(Exception e)
+		 {
+			e.printStackTrace();
+		 }}
+	 
+		void  connectDB() {
+			try {
+			con = Singleton.getConnection();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	
+	 public void selectAll(DefaultTableModel model) {
+			 sql = "select * from menu order by cno";
+			try {
+				pstmt = con.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+				for(int i =0; i< model.getRowCount();) {
+					model.removeRow(0);
+				}
+				while (rs.next()) {
+				Object[] data = { rs.getString("cno"),rs.getString("cname"),
+				 rs.getInt("cprice")
+				 };
+				 model.addRow(data);
+				}
+				
+			} catch (SQLException se) {
+				System.out.println("selectAll : " + se.toString());
+			} finally {
+			
+			}
+	}
+	
+	 public void SearchCname(DefaultTableModel model, String cName) {
+			sql = "select * from menu where  cname like '%' || ? || '%' order by cno";
+		try {
+			for(int i =0; i< model.getRowCount();) {
+				model.removeRow(0);
+			}
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, cName);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Object[] data = { rs.getString("cno"),rs.getString("cname"),
+				 rs.getInt("cprice")
+				 };
+				 model.addRow(data);
+			}
+		} catch(Exception e) {
+			System.out.println("selectcname" + e);
+		}
+	}
+	 void InsertCname(String cno, String cname, Integer cprice) {
+		sql = "insert into menu values(?, ?, ?)";
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, cno);
+			pstmt.setString(2, cname);
+			pstmt.setInt(3, cprice);
+			pstmt.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	 public void DeleteMyInfo(String cname) {
+		try {
+		sql = "delete from menu where cname = ?";
+		pstmt = con.prepareStatement(sql);
+		pstmt.setString(1, cname);
+		pstmt.executeUpdate();
+		}catch(Exception e) { 
+			e.printStackTrace();
+		}
+	}
+	
+	 public void UpdateMyInfo(MenuDTO dto) {
+		try {
+			sql ="update menu set cno = ?, cname = ?, cprice = ?";
+		} catch(Exception e) {
+			System.out.println("111");
+		}
+	}
+
+	
+
+}
